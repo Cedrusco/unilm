009304db6dd8067bbc6cb2e11d585d2e9a470427
Sarah Bacha <sarah.bacha94@gmail.com>
2020-06-15 10:28:04 +0300

    add mapping

---
+++ layoutlm/examples/classification/mapping.py
@@ -0,0 +1,29 @@
+
+import pandas as pd
+mapping_df = pd.read_csv('mapping.csv')
+
+
+def get_label(template_id):
+    row = mapping_df[mapping_df["template_id"] == template_id] 
+    label = row["label"].values[0]
+    print("get_label ", label)
+    return label
+
+def check_if_exists(template_id):
+    template_ids = mapping_df["template_id"].values
+    exists = template_id in template_ids
+    print("check_if_exists ", exists)
+    return exists
+
+def add_template_id(template_id):
+    print(mapping_df)
+    max_label = mapping_df['label'].max()
+    new_label = int(max_label) +1
+    new_row = [new_label ,template_id]
+    mapping_df.loc[len(mapping_df)] = new_row
+    mapping_df.to_csv('mapping.csv', header=True, mode = 'w', index=False)
+    return new_label
+
+
+if __name__ == "__main__":
+    add_template_id('temp_2')
