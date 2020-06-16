11e4d53b47c005fc256eca28c3eb89d245d63271
Sarah Bacha <sarah.bacha94@gmail.com>
2020-06-16 10:30:04 +0300

    put back old convert

--- layoutlm/layoutlm/data/convert.py
+++ layoutlm/layoutlm/data/convert.py
@@ -10,17 +10,17 @@ def convert_all_to_xml(root):
         for file in files:  # walk through dataset for img files
             if file.endswith('.png') | file.endswith('.jpg') | file.endswith('.tiff') | file.endswith('.tif'):
                 img = os.path.join(path, file)
-                convert_img_to_xml(img, output_dir,os.path.splitext(img)[0])
+                convert_img_to_xml_for_all(img, output_dir)
 
-# def convert_img_to_xml(img, output_dir):
-#     img_path, img_file = os.path.split(img)
-#     output_file = os.path.join(output_dir, os.path.splitext(img_file)[0])
-#     subprocess.run(['tesseract', img, output_file, 'hocr']) # make call to Tesseract with img file and output dir
-#     old_file_path = output_file + '.hocr'
-#     new_file_path = output_file + '.xml'
-#     if not os.path.exists(new_file_path):
-#         shutil.copy(old_file_path, new_file_path) # copy .hocr file contents into .xml file
-#         os.remove(old_file_path) # delete original .hocr file
+def convert_img_to_xml_for_all(img, output_dir):
+    img_path, img_file = os.path.split(img)
+    output_file = os.path.join(output_dir, os.path.splitext(img_file)[0])
+    subprocess.run(['tesseract', img, output_file, 'hocr']) # make call to Tesseract with img file and output dir
+    old_file_path = output_file + '.hocr'
+    new_file_path = output_file + '.xml'
+    if not os.path.exists(new_file_path):
+        shutil.copy(old_file_path, new_file_path) # copy .hocr file contents into .xml file
+        os.remove(old_file_path) # delete original .hocr file
 
 def convert_img_to_xml(img, output_dir, filename):
     # img_path, img_file = os.path.split(img)
