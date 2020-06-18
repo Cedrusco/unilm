e101b65cbc7c8c55b1631305c64a2d9a5eb9c17f
mariannekhanfour1 <marianne@cedrus.digital>
2020-06-18 15:38:21 +0300

    add validation

--- layoutlm/examples/classification/app.py
+++ layoutlm/examples/classification/app.py
@@ -3,6 +3,8 @@ import os
 from flask import Flask, request, jsonify, abort
 from waitress import serve
 from flask_cors import CORS
+import base64
+import binascii
 import sys
 ROOT_DIR = os.path.abspath("../../../")
 sys.path.append(ROOT_DIR)
@@ -16,8 +18,15 @@ from multiprocessing import Process
 def predict_label():
     if not request.json or not 'img' in request.json:
         abort(400)
+    body_img = request.json.get("img")
+    if not body_img:
+        abort(400)
+    try:
+        base64.b64decode(body_img, '-_')
+    except:
+        return "Not a base-64", 404
     matches = request.json['numMatches'] if 'numMatches' in request.json else 1
-    response = predict(request.json['img'], matches)
+    response = predict(body_img, matches)
     return jsonify(response), 201
 @app.route('/train', methods=['POST'])
 def train_label():
@@ -29,6 +38,10 @@ def train_label():
     body_template_id = request.json.get("template_id")
     if not body_img or not body_template_id or body_img.isspace() or body_template_id.isspace():
         abort(400)
+    try:
+        base64.b64decode(body_img, '-_')
+    except:
+        return "Not a base-64", 404
     task_training = Process(target=do_training, args=( body_img, body_template_id))
     task_training.start()
     return "Training in process!", 202
