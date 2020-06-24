9b7f19c7ad12c4ffb9a177cbe899f8aaada1d2e7
Chris <chris.xu@cedrusco.com>
2020-06-24 13:27:33 -0400

    Refactored convert.py and added file and method descriptions

--- layoutlm/layoutlm/data/convert.py
+++ layoutlm/layoutlm/data/convert.py
@@ -1,7 +1,15 @@
 import os, sys, subprocess, shutil
 
+# This script can be run to pass each image within a dataset through tesseract-OCR to generate HOCR format .xml files
+# Navigate to the directory of this file and run command "python3 convert.py path/to/dataset/"
+# Also provides method convert_img_to_xml to be used in prediction and training scripts
+
+OUT_DIR = '/out-OCR/'
+
+# Convert an entire directory of images and generate .xml files in new directory ~/out-OCR/
+# Expects one argument, root - string path to image directory
 def convert_all_to_xml(root):
-    output_dir = root + '/out-OCR/'
+    output_dir = root + OUT_DIR
     try:
         os.mkdir(output_dir) # make output directory if it does not exist already
     except:
@@ -10,21 +18,14 @@ def convert_all_to_xml(root):
         for file in files:  # walk through dataset for img files
             if file.endswith('.png') | file.endswith('.jpg') | file.endswith('.tiff') | file.endswith('.tif'):
                 img = os.path.join(path, file)
-                convert_img_to_xml_for_all(img, output_dir)
+                convert_img_to_xml(img, output_dir, os.path.splitext(file)[0]) # convert single img
 
-def convert_img_to_xml_for_all(img, output_dir):
-    img_path, img_file = os.path.split(img)
-    output_file = os.path.join(output_dir, os.path.splitext(img_file)[0])
-    subprocess.run(['tesseract', img, output_file, 'hocr']) # make call to Tesseract with img file and output dir
-    old_file_path = output_file + '.hocr'
-    new_file_path = output_file + '.xml'
-    if not os.path.exists(new_file_path):
-        shutil.copy(old_file_path, new_file_path) # copy .hocr file contents into .xml file
-        os.remove(old_file_path) # delete original .hocr file
 
+# Convert a single image to HOCR format .xml file
+# Expects three arguments, img - path to image to convert,
+# output_dir - directory to generate output files,
+# filename - name of output file
 def convert_img_to_xml(img, output_dir, filename):
-    # img_path, img_file = os.path.split(img)
-    # output_file = os.path.join(output_dir, os.path.splitext(img_file)[0])
     output_file = os.path.join(output_dir, filename)
     subprocess.run(['tesseract', img, output_file, 'hocr']) # make call to Tesseract with img file and output dir
     old_file_path = output_file + '.hocr'
