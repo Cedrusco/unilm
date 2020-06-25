7371da10c10d2346a169839e4bdae35deef1ff89
mariannekhanfour1 <marianne@cedrus.digital>
2020-06-25 16:38:46 +0300

    added all requirements

--- layoutlm/setup.py
+++ layoutlm/setup.py
@@ -19,6 +19,10 @@ setup(
         "lxml==4.5.1",
         "seqeval==0.0.12",
         "Pillow==7.1.2",
+        "Flask==1.1.2",
+        "waitress==1.4.4",
+        "flask_cors==3.0.8",
+        "pandas==1.0.5"
     ],
     extras_require={
         "dev": ["flake8==3.8.2", "isort==4.3.21", "black==19.10b0", "pre-commit==2.4.0"]
